3ca108f7817cde23afdfe7e644169b816a56c1b8
Juan Luis Romero <juanlu_21@hotmail.com>
2017-10-25 00:19:03 +0200

    todo arreglado, para pull request (?)

--- src/js/prueba.spec.js
+++ src/js/prueba.spec.js
@@ -2,10 +2,10 @@
 const { expect } = require("chai");
 
 // Dependencias del proyecto
-const Recipe = require("./Recipe");
+const sumar = require("./prueba");
 
-describe("La prueba para Recipe.js", () => {
-	it("deberia instanciar correctamente la el objeto de tipo Recipe", () => {
+describe("El archivo prueba.js", () => {
+	it("debería sumar números correctamente", () => {
 		expect(sumar(1, 2)).to.equal(3);
 	});
 });
