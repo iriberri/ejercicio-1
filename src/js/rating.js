06257cce88e88cfbf82d6a1bd35e6d1a1e0e9333
Marcobr93 <marcoabr93@gmail.com>
2017-10-25 02:09:08 +0200

    Comentarios añadidos en rating.js y cambios en los tets de rating.spec.js

--- src/js/rating.js
+++ src/js/rating.js
@@ -2,28 +2,42 @@
  * Clase Rating que se compone de idRating, name, score, idAuthor y idRecipe
  */
 class Rating {
+	/**
+	 * Constructor que recibe todos los parámetros. En él controlamos el tipo de parámetro,
+	 * por si recibe uno incorrecto, poder lanzar una excepción.
+	 * @param idRating Integer
+	 * @param name String
+	 * @param score Integer
+	 * @param idAuthor Integer
+	 * @param idRecipe Integer
+	 */
 	constructor(idRating, name, score, idAuthor, idRecipe) {
-		const tipoDatoInvalido = "El tipo de datos es inválido";
+		const tipoDatoInvalido = "El tipo de datos es no es válido";
+		// Controlamos que el número introducido sea un número y que este no sea menor que 0.
 		if (typeof idRating !== "number" || idRating < 0) {
 			throw tipoDatoInvalido;
 		} else {
 			this.idRating = idRating;
 		}
+		// Controlamos que el nombre introducido sea un stringy que este no sea una cadena vacía.
 		if (typeof name !== "string" || name.length <= 0) {
 			throw tipoDatoInvalido;
 		} else {
 			this.name = name;
 		}
+		// Controlamos que el score introducido sea un número y que este no sea menor que 0.
 		if (typeof score !== "number" || score < 0) {
 			throw tipoDatoInvalido;
 		} else {
 			this.score = score;
 		}
+		// Controlamos que el idAuthor introducido sea un número y que este no sea menor que 0.
 		if (typeof idAuthor !== "number" || idAuthor < 0) {
 			throw tipoDatoInvalido;
 		} else {
 			this.idAuthor = idAuthor;
 		}
+		// Controlamos que el idRecipe introducido sea un número y que este no sea menor que 0.
 		if (typeof idRecipe !== "number" || idRecipe < 0) {
 			throw tipoDatoInvalido;
 		} else {
--- src/js/rating.spec.js
+++ src/js/rating.spec.js
@@ -1,55 +1,35 @@
+// Dependencias externas
 const { expect } = require("chai");
 
+// Dependencias del proyecto
 const Rating = require("./rating");
-// Parámetros que debe recibir: idRating, name, score, idAuthor, idRecipe
-const rating = new Rating(1, "Marco", 7, 4, 3);
-// const rating2 = new Rating(null, "Marco", 7, 4, 3);
-// const rating3 = new Rating(1, 8, 7, 4, 3);
 
+let rating;
+let ratingError;
 
-describe("rating.idRating", () => {
-	it("deberia devolver 1", () => {
-		expect(rating.idRating).to.equal(1);
+describe("Rating", () => {
+	describe("idRating, name, score, idAuthor, idRecipe", () => {
+		before(() => { rating = new Rating(1, "Marco", 5, 3, 75); });
+		it("idRating debería funcionar correctamente", () => {
+			expect(rating.idRating).to.equal(1);
+		});
+		it("name debería funcionar correctamente", () => {
+			expect(rating.name).to.equal("Marco");
+		});
+		it("score debería funcionar correctamente", () => {
+			expect(rating.score).to.equal(5);
+		});
+		it("idAuthor debería funcionar correctamente", () => {
+			expect(rating.idAuthor).to.equal(3);
+		});
+		it("idRecipe debería funcionar correctamente", () => {
+			expect(rating.idRecipe).to.equal(75);
+		});
 	});
-});
-
-describe("rating.name", () => {
-	it("deberia devolver Marco", () => {
-		expect(rating.name).to.equal("Marco");
+	describe("Excepciones", () => {
+		ratingError = () => new Rating("Marco", 2, -5, null, undefined);
+		it("Debería lanzar una excepción si se le pasa un atributo erróneo", () => {
+			expect(ratingError).to.throw();
+		});
 	});
-});
-
-describe("rating.score", () => {
-	it("deberia devolver 7", () => {
-		expect(rating.score).to.equal(7);
-	});
-});
-
-describe("rating.idAuthor", () => {
-	it("deberia devolver 4", () => {
-		expect(rating.idAuthor).to.equal(4);
-	});
-});
-
-describe("rating.idRecipe", () => {
-	it("deberia devolver 3", () => {
-		expect(rating.idRecipe).to.equal(3);
-	});
-});
-/*
-describe("rating2.idRating", () => {
-	it("deberia lanzar una excepción por ser de un tipo distinto", () => {
-		expect(rating2.idRating).to.equal(1);
-	});
-});
-
-describe("rating3.name", () => {
-	it("deberia lanzar una excepción por ser de un tipo distinto", () => {
-		expect(rating3.name).to.equal("Marco");
-	});
-});
-*/
-
-describe("rating2", () => {
-	it("deberia devolver una excepcion");
 });
