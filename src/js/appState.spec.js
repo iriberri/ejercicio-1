7420163811c82d1d99241ef500a002515edc5f74
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-10-28 16:31:56 +0200

    Añadido appState
    AppState es un archivo que contiene el estado de la aplicación, y proporciona funciones para alterarlo.

---
+++ src/js/appState.spec.js
@@ -0,0 +1,110 @@
+// Chai usa accesores para invocar funciones (...that.is.empty), por lo tanto
+// esto no es compatible con la regla no-unused-expressions
+/* eslint-disable no-unused-expressions */
+
+// Dependencias externas
+const { expect } = require("chai");
+
+// Dependencias del proyecto
+const Rating = require("./rating");
+const Recipe = require("./recipe");
+const User = require("./user");
+const Ingredient = require("./ingredient");
+const {
+	getState,
+	addRecipeToState,
+	addRatingToState,
+	addUserToState,
+} = require("./appState");
+
+describe("appState", () => {
+	let dummyRecipe;
+	let dummyIngredients;
+	let dummySteps;
+	let dummyUser;
+	let dummyRating;
+
+	before(() => {
+		dummyIngredients = [
+			new Ingredient(1, "leche"),
+			new Ingredient(2, "yogu"),
+			new Ingredient(3, "porvo"),
+			new Ingredient(4, "asuca"),
+		];
+		dummySteps = ["a", "b", "c", "d", "f", "g"];
+		dummyRecipe	= new Recipe(
+			1,
+			dummyIngredients,
+			dummySteps,
+			"mexicana",
+			"china",
+			1,
+		);
+		dummyUser = new User(1, "Pablo");
+		dummyRating = new Rating(
+			1,
+			"Me encanta",
+			10,
+			1,
+			1,
+		);
+	});
+
+	it("debería crear un estado vacío al inicializarse", () => {
+		const state = getState();
+
+		expect(state.recipes).to.be.an("Array").that.is.empty;
+		expect(state.ratings).to.be.an("Array").that.is.empty;
+		expect(state.users).to.be.an("Array").that.is.empty;
+	});
+
+	describe("Funciones para añadir elementos", () => {
+		it("addRecipeToState debería añadir recetas", () => {
+			addRecipeToState(dummyRecipe);
+			expect(getState().recipes).to.contain(dummyRecipe);
+		});
+
+		it(
+			"addRecipeToState debería rechazar objetos que no son recetas",
+			() => {
+				expect(() => addRecipeToState(null)).to.throw();
+				expect(() => addRecipeToState()).to.throw();
+				expect(() => addRecipeToState({ hola: "mundo" })).to.throw();
+				expect(() => addRecipeToState(dummyRating)).to.throw();
+				expect(() => addRecipeToState(dummyUser)).to.throw();
+			},
+		);
+
+		it("addRatingToState debería añadir valoraciones", () => {
+			addRatingToState(dummyRating);
+			expect(getState().ratings).to.contain(dummyRating);
+		});
+
+		it(
+			"addRatingToState debería rechazar objetos que no son valoraciones",
+			() => {
+				expect(() => addRatingToState(null)).to.throw();
+				expect(() => addRatingToState()).to.throw();
+				expect(() => addRatingToState({ hola: "mundo" })).to.throw();
+				expect(() => addRatingToState(dummyRecipe)).to.throw();
+				expect(() => addRatingToState(dummyUser)).to.throw();
+			},
+		);
+
+		it("addUserToState debería añadir usuarios", () => {
+			addUserToState(dummyUser);
+			expect(getState().users).to.contain(dummyUser);
+		});
+
+		it(
+			"addUserToState debería rechazar objetos que no son usuarios",
+			() => {
+				expect(() => addUserToState(null)).to.throw();
+				expect(() => addUserToState()).to.throw();
+				expect(() => addUserToState({ hola: "mundo" })).to.throw();
+				expect(() => addUserToState(dummyRecipe)).to.throw();
+				expect(() => addUserToState(dummyRating)).to.throw();
+			},
+		);
+	});
+});
