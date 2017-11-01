8d3369a874a13fde4e5813d2e3e97c59101fd2b9
Marcobr93 <marcoabr93@gmail.com>
2017-11-01 18:03:13 +0100

    Terminada la parte de comparar dos recetas

--- src/js/pages/search/getRecipesBySimilarRecipe.js
+++ src/js/pages/search/getRecipesBySimilarRecipe.js
@@ -2,19 +2,16 @@
 const { getState } = require("../../appState");
 
 /**
- * Con esta funcion buscamos todas aquellas recetas que sean similar por contener
- * un nombre, ingredientes o tipo de comida similar a la receta introducida.
- * @param similarRecipe String valor a buscar
+ * Función que devolverá true o false dependiendo de si las recetas comparadas son similares o no
+ * @param receta
+ * @param similarRecipe
+ * @returns {boolean}
  */
-function getRecipesBySimilarRecipe(similarRecipe) {
-	/**
-	 * Recogemos las recetas qu tenemos
-	 */
-	const recetas = getState().recipes;
+function isSimilar(receta, similarRecipe) {
 	/**
 	 * Obtenemos cuantos ingredientes tiene la receta
 	 */
-	const numIngredientes = recetas.ingredient.length;
+	const numIngredientes = receta.ingredient.length;
 	/**
 	 * Obtenemos cuantos ingredientes tiene la receta introducida para comparar
 	 */
@@ -28,7 +25,7 @@ function getRecipesBySimilarRecipe(similarRecipe) {
 	 * Al ser el nombre de la receta un String, lo convertiremos
 	 * a minúscula y lo separaremos
 	 */
-	recetas.name.toLowerCase().split(" ");
+	receta.name.toLowerCase().split(" ");
 	/**
 	 * Al ser el nombre de la receta recibida un String, lo convertiremos
 	 * a minúscula y lo separaremos
@@ -37,7 +34,7 @@ function getRecipesBySimilarRecipe(similarRecipe) {
 	/**
 	 * Obtenemos cuantos palabras tiene la receta
 	 */
-	const numPalabras = recetas.name.length;
+	const numPalabras = receta.name.length;
 	/**
 	 * Obtenemos cuantos palabras tiene la receta introducida para comparar
 	 */
@@ -48,24 +45,20 @@ function getRecipesBySimilarRecipe(similarRecipe) {
 	 */
 	let numCoincidenciasPalabras = 0;
 	/**
-	 * Guardaremos aqui la puntuació obtenida en cada campo para saber si una receta es similar a otra
+	 * Guardaremos aqui la puntuación obtenida en cada campo para saber si una receta
+	 * es similar a otra
 	 * @type {number}
 	 */
 	let puntuacion = 0;
-	/**
-	 * Guardaremos aqui la receta que sea similar
-	 * @type {Array}
-	 */
-	const recetaSimilar = [];
 	/**
 	 * Doble bucle for of donde recorreremos los ingredientes
 	 * de ambas recetas y los iremos comparando, si coinciden
 	 * los ingreditentes, sumaremos 1 a la variable numCoincidenciasIngreientes
 	 */
 	/* eslint-disable */
-	for (const ingredientes of recetas.ingredient) {
+	for (const ingredientes of receta.ingredient) {
 		for (const ingredientes2 of similarRecipe.ingredient) {
-			if (ingredientes.ingredient === ingredientes2.ingredient) {
+			if (ingredientes.ingredient.id === ingredientes2.ingredient.id) {
 				numCoincidenciasIngredientes++;
 			}
 		}
@@ -75,7 +68,7 @@ function getRecipesBySimilarRecipe(similarRecipe) {
 	 * de ambas recetas y los iremos comparando, si coinciden
 	 * los nombres, sumaremos 1 a la variable numCoincidenciasPalabras
 	 */
-	for (const palabras of recetas.name) {
+	for (const palabras of receta.name) {
 		for (const palabras2 of similarRecipe.name) {
 			if (palabras === palabras2) {
 				numCoincidenciasPalabras++;
@@ -84,37 +77,79 @@ function getRecipesBySimilarRecipe(similarRecipe) {
 	}
 	/* eslint-enable */
 	/**
-	 * Regla de 3 para saber si la coincidencia de los ingredientes es >= 50%, en caso afirmativo,
-	 * sumamos 1 a la puntuación.
+	 * Definimos una constatne que será una regla de 3 que nos peritirá saber el % de similitud entre
+	 * dos recetas en cuanto a la comparación de sus ingredientes
+	 * @type {number}
 	 */
-	if (((numCoincidenciasIngredientes * 100) / ((numIngredientes + numIngredientes2) / 2)) >= 50) {
+	const operacionIngredientes = (numCoincidenciasIngredientes * 100) /
+		((numIngredientes + numIngredientes2) / 2);
+	/**
+	 * Dependiendo del resultado obtenido, la puntuación será mayor o menor
+	 */
+	if (operacionIngredientes >= 50 && operacionIngredientes < 75) {
+		puntuacion += 0.5;
+	} else if (operacionIngredientes >= 75 && operacionIngredientes < 100) {
+		puntuacion += 0.75;
+	} else if (operacionIngredientes === 100) {
 		puntuacion += 1;
 	}
 	/**
-	 * Regla de 3 para saber si la coincidencia de los nombres es >= 50%, en caso afirmativo,
-	 * sumamos 1 a la puntuación.
+	 * Definimos una constatne que será una regla de 3 que nos peritirá saber el % de similitud entre
+	 * dos recetas en cuanto a la comparación de su nombre
+	 * @type {number}
 	 */
-	if (((numCoincidenciasPalabras * 100) / ((numPalabras + numPalabras2) / 2)) >= 50) {
+	const operacionPalabras = (numCoincidenciasPalabras * 100) /
+		((numPalabras + numPalabras2) / 2);
+	/**
+	 * Dependiendo del resultado obtenido, la puntuación será mayor o menor
+	 */
+	if (operacionPalabras >= 50 && operacionPalabras < 75) {
+		puntuacion += 0.5;
+	} else if (operacionPalabras >= 75 && operacionPalabras < 100) {
+		puntuacion += 0.75;
+	} else if (operacionPalabras === 100) {
 		puntuacion += 1;
 	}
 	/**
 	 * Comprobamos si el tipo de comida son iguales en ambas recetas y en caso afirmativo,
 	 * sumamos 1 a la puntuación.
 	 */
-	if (recetas.typeOfFood === similarRecipe.typeOfFood) {
+	if (receta.typeOfFood === similarRecipe.typeOfFood) {
 		puntuacion += 1;
 	}
 	/**
-	 * Es la condición que he establecido para que una receta sea similar a otra, la puntuación deberá
-	 * ser >= 2 y al Array recetaSimilar, le añadimos la receta que acabamos de comparar.
+	 * La condición que he establecido para que dos recetas sean parecidas, es que el mínimo de
+	 * puntuación sea de 1.5, donde el máximo es 3, lo cual significaría que dos recetas
+	 * son iguales. Devolveremos las recetas cuya puntuación sea >= a 1.5
 	 */
-	if (puntuacion >= 2) {
-		recetaSimilar.push(recetas);
-	}
+	return puntuacion >= 1.5;
+}
+
+/**
+ * Con esta funcion buscaremos en el array de recetas, aquellas que sean similares
+ * @param similarRecipe String valor a buscar
+ */
+function getRecipesBySimilarRecipe(similarRecipe) {
 	/**
-	 * Devolvemos la receta
+	 * Recogemos las recetas qu tenemos
 	 */
-	return recetaSimilar;
+	const recetas = getState().recipes;
+	/**
+	 * Creamos el array que contendrá las recetas similares
+	 * @type {Array}
+	 */
+	const arraySimilarRecipes = [];
+	/* eslint-disable */
+	/**
+	 * Bucle for in que recorrerá el array de recetas e iremos aplicando la función isSimilar para saber si hay recetas
+	 * parecidas, en caso afirmativo, añadimos esa receta a arraySimilarRecipes
+	 */
+	for (const receta in recetas) {
+		if (isSimilar(receta, similarRecipe)) {
+			arraySimilarRecipes.push(receta);
+		}
+	}
+	/* eslint-enable */
 }
 
 module.exports = getRecipesBySimilarRecipe.js;
