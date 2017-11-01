c3b721e58fd3e63650f55e2e46dcb72a6159308d
Juan Luis Romero <juanlu_21@hotmail.com>
2017-11-01 20:52:37 +0100

    A falta de probar tests y hacer más tests 'no permitidos nulos'

--- src/js/pages/edit/addOrEditStepInRecipe.js
+++ src/js/pages/edit/addOrEditStepInRecipe.js
@@ -1,4 +1,11 @@
 function addOrEditStepInRecipe(recipe, newStep, existingStepIndex) {
+	const tipoDatoInvalido = "Tipo de dato invalido";
+	if (typeof newStep !== typeof "") {
+		throw new Error(tipoDatoInvalido);
+	}
+	if (typeof existingStepIndex !== typeof 1 || existingStepIndex !== undefined) {
+		throw new Error(tipoDatoInvalido);
+	}
 	const newRecipe = recipe;
 	if (existingStepIndex === undefined) {
 		newRecipe.steps.push(newStep);
