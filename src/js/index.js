712d8070466b386d78e2da007da05a0d75016932
Marcobr93 <marcoabr93@gmail.com>
2017-11-03 03:39:46 +0100

    No encuentro la forma de pasar un id y obtener la receta que le corresponda sin que sea un array

--- src/js/index.js
+++ src/js/index.js
@@ -36,5 +36,34 @@ window.addEventListener("load", () => {
 		"Italia",
 		1
 	);
+	const recipe2 = new Recipe(
+		1,
+		"Macarrones a la boloñesa",
+		[
+			{
+				ingredient: new Ingredient(1, "Macarrones"),
+				quantity: "200 g"
+			},
+			{
+				ingredient: new Ingredient(2, "Boloñesa"),
+				quantity: "10 g"
+			},
+			{
+				ingredient: new Ingredient(3, "Mucho amor"),
+				quantity: "1000 g"
+			},
+		],
+		[
+			"Cogemos los macarrones",
+			"Les echamos boloñesa",
+			"Sírvase en un plato hondo",
+			"Y pongo esto porque...",
+			"...Jorge quiere que pongamos 5 pasos",
+		],
+		"Primer plato",
+		"Italia",
+		1
+	);
 	addRecipeToState(recipe);
+	addRecipeToState(recipe2);
 });
