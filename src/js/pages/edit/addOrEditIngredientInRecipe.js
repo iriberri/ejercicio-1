9377b0b33f0759ed649f045f24c2ece75454b8c0
Juan Luis Romero <juanlu_21@hotmail.com>
2017-11-03 01:41:03 +0100

    añadidos comentarios

--- src/js/pages/edit/addOrEditIngredientInRecipe.js
+++ src/js/pages/edit/addOrEditIngredientInRecipe.js
@@ -1,19 +1,27 @@
 const Ingredient = require("../../model/Ingredient");
-
+// La funcion debe de devolvernos una receta con un ingrediente más o
+// un engrediente actualizado en el caso de que el index pasado por parámetro exista.
 function addOrEditIngredientInRecipe(recipe, newIngredient, existingIngredientIndex) {
 	const tipoDatoInvalido = "Tipo de dato invalido";
+	const newRecipe = recipe;
+	// Comprobamos que lo que pasamos por parámetro sea un ingrediente y que su cantidad sea un String
 	if (!(newIngredient.ingredient instanceof Ingredient) || typeof newIngredient.quantity !== typeof "") {
 		throw new Error(tipoDatoInvalido);
 	}
+	// Comprobamos que el index pasado por parámetro sea un número o indefinido
 	if (typeof existingIngredientIndex !== typeof 1 || existingIngredientIndex !== undefined) {
 		throw new Error(tipoDatoInvalido);
 	}
-	const newRecipe = recipe;
-	if (existingIngredientIndex === undefined) {
+	// Si el index es indefinido o sea mayor o igual que el numero de ingredientes,
+	// lo añadimos a la receta como un ingrediente más
+	if (existingIngredientIndex === undefined ||
+		existingIngredientIndex >= newRecipe.ingredients.length) {
 		newRecipe.ingredients.push(newIngredient);
 	} else {
+		// Si el index existe, actualizamos el ingrediente que corresponda a la posición indicada
 		newRecipe.ingredients[existingIngredientIndex] = newIngredient;
 	}
+	// Devolvemos la receta actualizada.
 	return newRecipe;
 }
 
