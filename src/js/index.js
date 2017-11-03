b3a6410cb5654371026a39437dbf365a35f3a20e
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-11-03 22:50:35 +0100

    Terminado controlador de createRecipe

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 const loadSearchController = require("./controller/search");
+const loadEditControler = require("./controller/edit");
 const { loadStateFromStorage } = require("./storage");
 
 window.addEventListener("load", () => {
 	loadSearchController();
+	loadEditControler();
 	loadStateFromStorage();
 });
