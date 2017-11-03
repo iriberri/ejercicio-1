59f2c1226b2ad558d77145bfaeaa9ee812608c4e
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-11-03 14:18:50 +0100

    Cambios finales
    - La función ya no recibe el parámetro ID sino que lo calcula
    - Implementados los tests

--- src/js/pages/edit/createRecipe.js
+++ src/js/pages/edit/createRecipe.js
@@ -1,14 +1,13 @@
 /**
  * Created by Fran on 02/11/2017.
  */
-const { addRecipeToState } = require("../../appState");
+const { addRecipeToState, getState } = require("../../appState");
 const Recipe = require("../../model/Recipe");
 
 /**
  * Funcion que valida todos los campos de una receta para crearla, si recibe un campo de
  * tipo diferente, null o undefined, una receta con menos de 3 pasos o ingredientes repetidos
  * o una receta con menos de 5 pasos o pasos repetidos devolvera success = false y el tipo de error
- * @param recipeId Integer
  * @param name String
  * @param ingredients Array[Ingredient]
  * @param steps Array[String]
@@ -17,8 +16,9 @@ const Recipe = require("../../model/Recipe");
  * @param authorId String
  * @returns {{success: *, error: *}} boolean and Error
  */
-function createRecipe(recipeId, name, ingredients, steps, typeOfFood, origin, authorId) {
+function createRecipe(name, ingredients, steps, typeOfFood, origin, authorId) {
 	const repeatedIngredient = "La receta tiene algun ingrediente repetido";
+	let recipe;
 	let error;
 	let success;
 	// Se pasa el id de los ingredientes a un array result.
@@ -37,7 +37,11 @@ function createRecipe(recipeId, name, ingredients, steps, typeOfFood, origin, au
 		/* Si la receta recibe valores definira el tipo de error, sino la añade
 		 a las recetas de state */
 		try {
-			const recipe = new Recipe(recipeId, name, ingredients, steps, typeOfFood, origin, authorId);
+			// Asignamos un ID a la receta
+			const recipeId = getState().recipes
+				.map(it => it.idRecipe)
+				.reduce((el, acc) => (el > acc ? el : acc), 0) + 1;
+			recipe = new Recipe(recipeId, name, ingredients, steps, typeOfFood, origin, authorId);
 			success = true;
 			addRecipeToState(recipe);
 		} catch (e) {
@@ -49,7 +53,7 @@ function createRecipe(recipeId, name, ingredients, steps, typeOfFood, origin, au
 		success = false;
 		error = new Error(repeatedIngredient);
 	}
-	return { success, error };
+	return { success, recipe, error };
 }
 
 module.exports = createRecipe;
