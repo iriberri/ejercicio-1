216f8ed3391ae7d31eaeaec599b2b9afa82c8aa6
Juan Luis Romero <juanlu_21@hotmail.com>
2017-11-03 01:47:44 +0100

    añadidos comentarios

--- src/js/pages/edit/addOrEditStepInRecipe.js
+++ src/js/pages/edit/addOrEditStepInRecipe.js
@@ -1,17 +1,25 @@
+// La funcion debe de devolvernos una receta con un paso más o
+// un paso actualizado en el caso de que el index pasado por parámetro exista.
 function addOrEditStepInRecipe(recipe, newStep, existingStepIndex) {
 	const tipoDatoInvalido = "Tipo de dato invalido";
+	const newRecipe = recipe;
+	// Comprobamos que lo que pasamos por parámetro sea un String
 	if (typeof newStep !== typeof "") {
 		throw new Error(tipoDatoInvalido);
 	}
+	// Comprobamos que el index pasado por parámetro sea un número o indefinido
 	if (typeof existingStepIndex !== typeof 1 || existingStepIndex !== undefined) {
 		throw new Error(tipoDatoInvalido);
 	}
-	const newRecipe = recipe;
-	if (existingStepIndex === undefined) {
+	// Si el index es indefinido o sea mayor o igual que el numero de pasos,
+	// lo añadimos a la receta como un ingrediente más
+	if (existingStepIndex === undefined || existingStepIndex > newRecipe.steps.length) {
 		newRecipe.steps.push(newStep);
 	} else {
+		// Si el index existe, actualizamos el ingrediente que corresponda a la posición indicada
 		newRecipe.steps[existingStepIndex] = newStep;
 	}
+	// Devolvemos la receta actualizada.
 	return newRecipe;
 }
 
