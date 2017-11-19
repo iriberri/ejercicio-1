026c70bd0bfc95e230dac8f64afae3f4fc7ffb99
Marcobr93 <marcoabr93@gmail.com>
2017-11-19 22:45:07 +0100

    Faltaba añadir loadManageController en index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const loadSearchController = require("./controller/search");
 const loadEditControler = require("./controller/edit");
-const loadManageController = require("./controller/manage")
+const loadManageController = require("./controller/manage");
 const { loadStateFromStorage } = require("./storage");
 
 window.addEventListener("load", () => {
